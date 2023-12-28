309f76050903802ce1845112de5b1486a25fd24f
Alex Dametto <dametto.alex@gmail.com>
2023-12-28 10:33:41 +0100

    Added custom cn function (twMerge + classNames)

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,18 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { cn } from "../utils/cn";
 
 export default function Button({ className }) {
-   return <button className={`bg-pink-500 p-2 rounded-sm ${className}`}>Hello Button</button>
+  return (
+    <button className={cn("bg-pink-500", "rounded-sm", className)}>
+      Hello Button
+    </button>
+  );
 }
 
 Button.propTypes = {
-    className: PropTypes.string
-}
+  className: PropTypes.string,
+};
 
 Button.defaultProps = {
-    className: ""
-}
+  className: "",
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { Button, Text } from './components';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { Button, Text } from "./components";
 
 const App = () => {
-    return (
-        <div>
-            <Button />
-            <Text />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Button />
+      <Text />
+    </div>
+  );
+};
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
